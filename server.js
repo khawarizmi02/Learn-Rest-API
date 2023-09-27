6ccb984f4a11d3790da8a3aadfb4bbde89235083
khawarizmi02 <117525739+khawarizmi02@users.noreply.github.com>
2023-09-27 16:48:44 +0800

    2 files modified

--- server.js
+++ server.js
@@ -2,15 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const port = 5000;
 
 app.use(bodyParser.json());
 
 const sayHi = (req, res) => {
-  res.send('Hi!');
+  res.send('Hi Khawarizmi!');
 };
 
 app.get('/', sayHi);
 
-app.listen(5000, () => {
-  console.log(`Server is running on port 5000.`);
+app.post('/add', (req, res) => {
+  const { a, b } = req.body;
+  res.send(`The sum is: ${a + b}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port http://localhost:${port}`);
 });
